9b72c3baea8b33fba994a5ea00659e7ad311737b
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-09 06:11:18 +0500

    name print

--- get_fruits_name.py
+++ get_fruits_name.py
@@ -7,6 +7,17 @@ def get_frutis_name(data:str)->list:
     returns:
         list: list of fruits names
     """
-    return
+    rows=data.split('\n')
+    data=[]
+    l=[]
+    for row in rows[:-1]:
+        data.append(row.split(','))
+    for i in range(1,len(data)):
+        price=(data[i][0])
+        print(price)     
+    return price
+f=open('fruits.csv')
+print(get_frutis_name(f.read()))
+    
 
     
